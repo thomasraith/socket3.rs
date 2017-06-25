df22ddb03e944933761ec52e797643e480ea5217
Thomas Raith <thomas.raith@tum.de>
2017-06-25 18:21:28 +0200

    Added 1 test and CI setup

---
+++ tests/socket3.rs
@@ -0,0 +1,22 @@
+extern crate socket3;
+
+use socket3::raw::*;
+use std::io::Write;
+
+#[test]
+fn raw_socket_ipv6_test() {
+    let socket = Socket::new(Domain::IPV6, Protocol::RAW);
+
+    if let Err(error) = socket {
+        let error_code = error.raw_os_error().unwrap();
+
+        //Checks is the error means missing permissions (test requires root privileges)
+        assert_eq!(error_code, 1);
+        writeln!(&mut std::io::stderr(), "############################################");
+        writeln!(&mut std::io::stderr(), "# ✘  Missing root privileges: Test aborted #");
+        writeln!(&mut std::io::stderr(), "############################################");
+        return;
+    }
+
+    assert!(socket.is_ok());
+}
